97c706c8dd85f4a3b06e66176f7fe90e1c743db4
k-shinsuke <k-shinsuke@iij.ad.jp>
2025-05-02 15:06:12 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,22 +1,10 @@
 from flask import Flask
-import os
-
-PORT = 8080
-name = os.environ['NAME']
-if name == None or len(name) == 0:
-  name = "world"
-MESSAGE = "Hellow, " + name + "!"
-print("Message: '" + MESSAGE + "'")
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return "Hello, World from App Runner!"
 
-@app.route("/")
-def root():
-  print("Handling web request. Returning message.")
-  result = MESSAGE.encode("utf-8")
-  return result
-
-
-if __name__ == "__main__":
-  app.run(debug=True, host="0.0.0.0", port=PORT)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
