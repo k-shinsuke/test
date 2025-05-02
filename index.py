2526b8692b1b4e491bbaae6ba4f386ebec7e48a1
k-shinsuke <k-shinsuke@iij.ad.jp>
2025-05-02 17:10:33 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
 
-app = Flask(__name__)
+PORT = int(os.environ.get("PORT", 8080))  # App Runnerが使うポート
 
-@app.route('/')
-def hello():
-    return "Hello, World from App Runner!"
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello from Python without Flask!")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    server = HTTPServer(("", PORT), SimpleHandler)
+    print(f"Starting server on port {PORT}")
+    server.serve_forever()
